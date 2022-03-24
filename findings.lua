4e76bc3633eadc78b1a05ad4078f08b11d17a4fd
McDonaldsEnergy <73415483+McDonaldsEnergy@users.noreply.github.com>
2022-03-24 15:08:45 -0500

    Create findings.lua

---
+++ findings.lua
@@ -0,0 +1,3 @@
+-- Couldn't have been made possible without help of a few friends dedicated to this project. --
+-- If you ever ask what this is, I'm never going to give you a direct response. Don't worry about it, and keep going on your way. --
+0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50
